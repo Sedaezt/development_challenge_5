36e87e3bdfe717e3a4cecab151e2f7764a589502
Sedaezt <sedaezzat@icloud.com>
2024-05-23 13:59:12 +0200

    css start-page

---
+++ js/vraagjes.js
@@ -0,0 +1,71 @@
+export const quizLocations = [
+    {   
+        lat: 50.8503, lng: 4.3517, 
+        city: 'Brussel', 
+        question: 'Welke beroemde standbeeld wordt beschouwd als het symbool van de stad en staat bekend om zijn kleine formaat?', 
+        answer: 'Manneken Pis' 
+    },
+
+    { 
+        lat: 51.0543, lng: 3.7174, 
+        city: 'Gent', 
+        question: 'Wat is de bekendste waterloop die door het centrum van Gent stroomt?', 
+        answer: 'De Leie' 
+    },
+
+    { 
+        lat: 51.2194, lng: 4.4025, 
+        city: 'Antwerpen',
+        question: 'Wat is de traditionele naam voor het ambacht van het maken van handgemaakte producten met behulp van naald en draad, een ambacht dat vaak wordt geassocieerd met de rijke geschiedenis van Antwerpen?', 
+        answer: 'Handwerpen' 
+    },
+
+    { 
+        lat: 50.8798, lng: 4.7005, 
+        city: 'Leuven', 
+        question: 'Welke beroemde middeleeuwse universiteit, opgericht in 1425, bevindt zich in Leuven en is een van de oudste universiteiten ter wereld?', 
+        answer: 'Katholieke Universiteit Leuven' 
+    },
+
+    { 
+        lat: 51.2093, lng: 3.2247, 
+        city: 'Brugge', 
+        question: 'Welke beroemde toren in Brugge, gebouwd in de 13e eeuw, biedt een prachtig uitzicht over de stad en staat bekend om zijn klokkenspel?', 
+        answer: 'De Belfort' 
+    },
+
+    { 
+        lat: 51.0259, lng: 4.4776, 
+        city: 'Mechelen', 
+        question: 'Welke beroemde kathedraal in Mechelen, met een 97 meter hoge toren, is een UNESCO-werelderfgoed en staat bekend om zijn carillonmuziek?', 
+        answer: 'Sint-Romboutskathedraal' 
+    },
+
+    { 
+        lat: 50.8284, lng: 3.2649, 
+        city: 'Kortrijk', 
+        question: 'Welke beroemde slag vond plaats in Kortrijk in 1302, waarbij Vlaamse milities een beslissende overwinning behaalden op de Franse ridders?', 
+        answer: 'Guldensporenslag' 
+    },
+
+    { 
+        lat: 50.9311, lng: 5.3378, 
+        city: 'Hasselt', 
+        question: 'Welke bekende Hasseltse specialiteit, gemaakt van graan en kruiden, wordt vaak als een sterke drank gedronken en heeft zelfs een eigen museum in de stad?', 
+        answer: 'Jenever' 
+    },
+
+    { 
+        lat: 51.2146, lng: 2.928, 
+        city: 'Oostende', 
+        question: 'Welk bekend Belgisch stripfiguur, gecreëerd door striptekenaar Hergé, heeft een standbeeld aan de Oostendse kustpromenade?', 
+        answer: 'Kuifje' 
+    },
+
+    { 
+        lat: 50.6326, lng: 5.5797,
+        city: 'Luik', 
+        question: 'Wat is de bekendste jaarlijkse evenement in Luik, een folkloristisch evenement dat plaatsvindt op de derde zondag van juli?', 
+        answer: 'De Luikse Markt' 
+}
+];
